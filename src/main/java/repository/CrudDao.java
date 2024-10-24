19a007dfd82129a76b32d731a748fdeed5b73018
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2024-10-24 19:15:09 +0530

    new commit

--- src/main/java/repository/CrudDao.java
+++ src/main/java/repository/CrudDao.java
@@ -1,4 +1,11 @@
 package repository;
 
-public interface CrudDao {
+import javafx.collections.ObservableList;
+
+public interface CrudDao <T> extends SuperDao{
+    boolean save(T t);
+    boolean delete(String id);
+    ObservableList<T> getAll();
+    boolean update(T t);
+    T search(String id);
 }
