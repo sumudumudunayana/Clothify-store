ce023c99bf5251d08a35e9540577085c91fa5f80
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2024-10-24 16:05:39 +0530

    new commits

--- src/main/java/service/custom/impl/EmployeeServiceImpl.java
+++ src/main/java/service/custom/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package service.custom.impl;
 
 import dto.Employee;
+import javafx.collections.ObservableList;
 import service.custom.EmployeeService;
 
 public class EmployeeServiceImpl implements EmployeeService {
@@ -8,5 +9,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public boolean addEmployee(Employee employee) {
         return false;
+    }
+
+    @Override
+    public ObservableList<Employee> getAllEmployees() {
+        return null;
     }
 }
